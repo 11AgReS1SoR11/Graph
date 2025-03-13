b670eac870e481bad60161b7ef8c8ecff1b8bcdb
Plirest <k.efremova.kk@yandex.ru>
2025-03-13 21:21:48 +0300

    [GRAP-38] Implement Logger
    	-add singleton Logger
    	-build/log.log - file for logs
    	-logs format is [time][level][entity] msg
    
    Ref: GRAP-38

---
+++ Common/Logger/src/Logger.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+#include <mutex>
+#include <QString>
+
+#define LOG_INFO(entity, message) Logger::getInstance().log(Logger::Level::INFO, entity, message);
+#define LOG_WARN(entity, message) Logger::getInstance().log(Logger::Level::WARN, entity, message);
+#define LOG_ERROR(entity, message) Logger::getInstance().log(Logger::Level::ERROR, entity, message);
+
+inline constexpr auto FRONTEND_LOG = "FRONTEND";
+inline constexpr auto BACKEND_LOG = "BACKEND";
+inline constexpr auto TRANSLATOR_LOG = "TRANSLATOR";
+inline constexpr auto RETRANSLATOR_LOG = "RETRANSLATOR";
+inline constexpr auto PARSER_LOG = "PARSER";
+
+inline constexpr auto LOG_FILE_PATH = "build/log.log";
+
+class Logger
+{
+public:
+    enum class Level { INFO, WARN, ERROR };
+
+    static Logger& getInstance();
+
+    void log(Level level, std::string const& entity, std::string const& message);
+    void log(Level level, QString const& entity, QString const& message);
+
+private:
+    Logger();
+    ~Logger();
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+    std::ofstream m_logFile;
+    std::mutex m_mutex;
+};
