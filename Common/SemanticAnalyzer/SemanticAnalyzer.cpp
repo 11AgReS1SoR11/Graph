3136505b558b4bbc497722f21a25401b2faf0a57
maxonic228 <kromachmax2003@gmail.com>
2025-03-23 05:08:13 +0300

    Add implementation of checks errors for note object_decl and relation

--- Common/SemanticAnalyzer/SemanticAnalyzer.cpp
+++ Common/SemanticAnalyzer/SemanticAnalyzer.cpp
@@ -1,5 +1,145 @@
 #include "SemanticAnalyzer.h"
 
-SemanticAnalyzer::SemanticAnalyzer()
+
+void SEMANTICANALYZER::SemanticAnalyzer::semanticAnalysis(const std::vector<std::pair<std::string, std::any>> &programTree, int rowNumber)
 {
+    declaredObjects.clear();
+
+    for (const auto& [type, statement] : programTree)
+    {
+        if (type == OBJECT_DECL)
+        {
+            checkObjectDecl(std::any_cast<ObjectDecl>(statement), rowNumber);
+        }
+        else if (type == RELATION)
+        {
+            checkRelation(std::any_cast<Relation>(statement), rowNumber);
+        }
+        else if (type == NOTE)
+        {
+            checkNote(std::any_cast<Note>(statement), rowNumber);
+        }
+        else if (type == GRAPH)
+        {
+            checkGraph(std::any_cast<Graph>(statement), rowNumber);
+        }
+        else if (type == DOT_CLOUD)
+        {
+            checkDotCloud(std::any_cast<DotCloud>(statement), rowNumber);
+        }
+
+        rowNumber++;
+    }
+}
+
+
+void SEMANTICANALYZER::SemanticAnalyzer::checkPropertyValue(const Property &property, const ConstraintInfo &constraints, int rowNumber) const
+{
+    if (constraints.type == TYPE_NUMBER)
+    {
+        if (!isNumber(property.value))
+        {
+            throw SemanticError("Значение свойства " + property.key + " должно быть числом.", rowNumber);
+        }
+
+        int value = std::stoi(property.value);
+
+        if (constraints.min.has_value() && value < constraints.min.value())
+        {
+            throw SemanticError("Значение свойства " + property.key + " должно быть не меньше " +
+                                std::to_string(constraints.min.value()) + ".", rowNumber);
+        }
+
+        if (constraints.max.has_value() && value > constraints.max.value())
+        {
+            throw SemanticError("Значение свойства " + property.key + " должно быть не больше " +
+                                std::to_string(constraints.max.value()) + ".", rowNumber);
+        }
+    }
+}
+
+
+void SEMANTICANALYZER::SemanticAnalyzer::checkObjectDecl(const ObjectDecl &obj, int rowNumber)
+{
+    if (declaredObjects.count(obj.id))
+    {
+        throw SemanticError("Объект " + obj.id + " уже объявлен.", rowNumber);
+    }
+
+    declaredObjects.insert(obj.id);
+
+    std::vector<std::string> allowedProperties = COMMON_PROPERTIES;
+    auto specificProps = SHAPE_SPECIFIC_PROPERTIES.at(obj.shape);
+    allowedProperties.insert(allowedProperties.end(), specificProps.begin(), specificProps.end());
+
+    for (const Property& prop : obj.properties)
+    {
+        if (std::find(allowedProperties.begin(), allowedProperties.end(), prop.key) == allowedProperties.end())
+        {
+            throw SemanticError("Недопустимое свойство " + prop.key + " для фигуры " + obj.shape, rowNumber);
+        }
+
+        if (PROPERTY_CONSTRAINTS.count(prop.key))
+        {
+            checkPropertyValue(prop, PROPERTY_CONSTRAINTS.at(prop.key), rowNumber);
+        }
+    }
+}
+
+
+void SEMANTICANALYZER::SemanticAnalyzer::checkRelation(const Relation &rel, int rowNumber)
+{
+    if (!declaredObjects.count(rel.id1))
+    {
+        throw SemanticError("Объект " + rel.id1 + " не объявлен.", rowNumber);
+    }
+
+    if (!declaredObjects.count(rel.id2))
+    {
+        throw SemanticError("Объект " + rel.id2 + " не объявлен.", rowNumber);
+    }
+
+    for (const Property& prop : rel.properties)
+    {
+        if (std::find(COMMON_PROPERTIES.begin(), COMMON_PROPERTIES.end(), prop.key) == COMMON_PROPERTIES.end())
+        {
+            throw SemanticError("Недопустимое свойство " + prop.key + " для отношения.", rowNumber);
+        }
+    }
+}
+
+
+void SEMANTICANALYZER::SemanticAnalyzer::checkNote(const Note &note, int rowNumber)
+{
+    for (const Property& prop : note.properties)
+    {
+        if (std::find(COMMON_PROPERTIES.begin(), COMMON_PROPERTIES.end(), prop.key) == COMMON_PROPERTIES.end())
+        {
+            throw SemanticError("Недопустимое свойство " + prop.key + " для заметки.", rowNumber);
+        }
+    }
+}
+
+
+void SEMANTICANALYZER::SemanticAnalyzer::checkGraph(const Graph &graph, int rowNumber)
+{
+
+}
+
+
+void SEMANTICANALYZER::SemanticAnalyzer::checkDotCloud(const DotCloud &dotCloud, int rowNumber)
+{
+
+}
+
+
+bool SEMANTICANALYZER::SemanticAnalyzer::isNumber(const std::string &str) const
+{
+    try {
+        std::size_t pos;
+        std::stoi(str, &pos);
+        return pos == str.length();
+    } catch (...) {
+        return false;
+    }
 }
--- Common/SemanticAnalyzer/SemanticAnalyzer.h
+++ Common/SemanticAnalyzer/SemanticAnalyzer.h
@@ -2,19 +2,220 @@
 #define SEMANTICANALYZER_H
 
 #ifdef _WIN32
-  #ifdef SEMANTICANALYZER_EXPORTS
-    #define SEMANTICANALYZER_API __declspec(dllexport)
-  #else
-    #define SEMANTICANALYZER_API __declspec(dllimport)
-  #endif
+#ifdef SEMANTICANALYZER_EXPORTS
+#define SEMANTICANALYZER_API __declspec(dllexport)
 #else
-  #define SEMANTICANALYZER_API
+#define SEMANTICANALYZER_API __declspec(dllimport)
 #endif
+#else
+#define SEMANTICANALYZER_API
+#endif
+
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <map>
+#include <stdexcept>
+#include <limits.h>
+#include <algorithm>
+#include <any>
+#include <charconv>
+#include <optional>
+#include <string_view>
+
+
+#define OBJECT_DECL     "object_decl"
+#define RELATION        "relation"
+#define NOTE            "note"
+#define GRAPH           "graph"
+#define DOT_CLOUD       "dot_cloud"
+
+#define PROP_COLOR      "color"
+#define PROP_TEXT       "text"
+#define PROP_BORDER     "border"
+#define PROP_X          "x"
+#define PROP_Y          "y"
+#define PROP_SIZE_TEXT  "size_text"
+#define PROP_RADIUS     "radius"
+#define PROP_SIZE_A     "size_A"
+#define PROP_SIZE_B     "size_B"
+#define PROP_ANGLE      "angle"
+
+#define TYPE_NUMBER     "number"
+#define TYPE_STRING     "string"
+
+#define SHAPE_CIRCLE    "circle"
+#define SHAPE_RECTANGLE "rectangle"
+#define SHAPE_DIAMOND   "diamond"
+
+
+namespace SEMANTICANALYZER
+{
+
+struct Property
+{
+    std::string key;
+    std::string value;
+};
+
+struct ObjectDecl
+{
+    std::string shape;
+    std::string id;
+    std::vector<Property> properties;
+};
+
+struct Relation
+{
+    std::string id1;
+    std::string arrow;
+    std::string id2;
+    std::vector<Property> properties;
+};
+
+struct Note
+{
+    std::string id;
+    std::vector<Property> properties;
+};
+
+struct Graph
+{
+    std::string id;
+    std::vector<Property> properties;
+    std::vector<ObjectDecl> objects;
+    std::vector<Relation> relations;
+};
+
+struct DotCloud
+{
+    std::string id;
+    std::vector<Property> properties;
+    std::vector<ObjectDecl> objects;
+};
+
+struct ConstraintInfo
+{
+    std::string type;
+    std::optional<int> min;
+    std::optional<int> max;
+};
+
+
+inline static const std::vector<std::string> COMMON_PROPERTIES =
+{
+    PROP_COLOR,
+    PROP_TEXT,
+    PROP_BORDER,
+    PROP_X,
+    PROP_Y,
+    PROP_SIZE_TEXT
+};
+
+inline static const std::map<std::string, std::vector<std::string>> SHAPE_SPECIFIC_PROPERTIES =
+{
+    {SHAPE_CIRCLE,    {PROP_RADIUS}},
+    {SHAPE_RECTANGLE, {PROP_SIZE_A, PROP_SIZE_B}},
+    {SHAPE_DIAMOND,   {PROP_SIZE_A, PROP_SIZE_B, PROP_ANGLE}}
+};
+
+inline static const std::map<std::string, ConstraintInfo> PROPERTY_CONSTRAINTS =
+{
+    {PROP_RADIUS,    {TYPE_NUMBER, 0, std::nullopt}},
+    {PROP_SIZE_A,    {TYPE_NUMBER, 0, std::nullopt}},
+    {PROP_SIZE_B,    {TYPE_NUMBER, 0, std::nullopt}},
+    {PROP_ANGLE,     {TYPE_NUMBER, 0, 360}},
+    {PROP_COLOR,     {TYPE_STRING, std::nullopt, std::nullopt}},
+    {PROP_TEXT,      {TYPE_STRING, std::nullopt, std::nullopt}},
+    {PROP_BORDER,    {TYPE_STRING, std::nullopt, std::nullopt}},
+    {PROP_X,         {TYPE_NUMBER, std::nullopt, std::nullopt}},
+    {PROP_Y,         {TYPE_NUMBER, std::nullopt, std::nullopt}},
+    {PROP_SIZE_TEXT, {TYPE_NUMBER, 0, std::nullopt}}
+};
+
+class SemanticError: public std::runtime_error
+{
+public:
+    SemanticError(const std::string& message, int rowNumber)
+        : std::runtime_error("Ошибка: " + message + " Строка " + std::to_string(rowNumber)){}
+};
+
 
 class SEMANTICANALYZER_API SemanticAnalyzer
 {
 public:
-    SemanticAnalyzer();
+
+    static SemanticAnalyzer& getInstance()
+    {
+        static SemanticAnalyzer instance;
+        return instance;
+    }
+
+    /*!
+     * \brief Точка входа в семантический анализатор.
+     * \param programTree - набор Statement
+     * \param rowNumber - номер Statement для логирования(временное решение)
+     */
+    void semanticAnalysis(const std::vector<std::pair<std::string, std::any>>& programTree, int rowNumber = 1);
+
+    /*!
+     * \brief Сброс состояния анализатора.
+     */
+    void reset() { declaredObjects.clear(); }
+
+    /*!
+     * \brief  Функция возвращает все объявленные глобально объекты.
+     */
+    const std::set<std::string>& getDeclaredObjects() const { return declaredObjects; }
+
+private:
+
+    SemanticAnalyzer() = default;
+
+    SemanticAnalyzer(const SemanticAnalyzer&) = delete;
+    SemanticAnalyzer& operator=(const SemanticAnalyzer&) = delete;
+
+    /*!
+     * \brief Функция проверяется соответсвие заданным атрибутам в структуре ConstraintInfo.
+     */
+    void checkPropertyValue(const Property& property, const ConstraintInfo& constraints, int rowNumber) const;
+
+    /*!
+     * \brief Функция проверяет корректность Statement "Объявление объекта".
+     */
+    void checkObjectDecl(const ObjectDecl& obj, int rowNumber);
+
+    /*!
+     * \brief Функция проверяет корректность Statement "Отношение между объектами".
+     */
+    void checkRelation(const Relation& rel, int rowNumber);
+
+    /*!
+     * \brief Функция проверяет корректность Statement "Заметка".
+     */
+    void checkNote(const Note& note, int rowNumber);
+
+    /*!
+     * \brief Функция проверяет корректность Statement "Граф".
+     */
+    void checkGraph(const Graph& graph, int rowNumber);
+
+    /*!
+     * \brief Функция проверяет корректность Statement "Облако точек".
+     */
+    void checkDotCloud(const DotCloud& dotCloud, int rowNumber);
+
+    /*!
+     * \brief Функция проверяет, является ли строка целым числом.
+     */
+    bool isNumber(const std::string& str) const;
+
+private:
+    std::set<std::string> declaredObjects;
+
 };
+
+}
 
 #endif // SEMANTICANALYZER_H
