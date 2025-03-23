f81e250b29c2834e49a3213c26e6ea0780d3fa53
GachiBoy0706 <sazhin.dd@edu.spbstu.ru>
2025-03-23 18:42:31 +0300

    added one test, gonna add more soon :)

--- Common/Retranslator/test/RetranslatorTest.cpp
+++ Common/Retranslator/test/RetranslatorTest.cpp
@@ -1,6 +1,6 @@
 #include <QtTest/QtTest>
 #include <stdexcept>
-
+#include <iostream>
 #include "Retranslator.hpp"
 using namespace AST;
 class TestClass : public QObject
@@ -15,14 +15,146 @@ void TestClass::test_objectDecl()
 {
     std::unordered_map<std::string, Shape*> map;
     Circle circle;
-    circle.text = "circle";
+    circle.text = "circleTEXT";
     circle.x = 1;
     circle.y = 1;
 
     Shape* circle_ptr = &circle;
-    map["circle"] = circle_ptr;
+    map["circleID"] = circle_ptr;
 
     ASTTree tree(Retranslator::getInstance().parseTree(map));
     auto it = tree.begin();
     QVERIFY(it->getValue() == "root");
+    ++it;
+    QVERIFY(it->getValue() == "STARTGRAPH");
+    ++it;
+    QVERIFY(it->getValue() == "STATEMENT");
+    ++it;
+    QVERIFY(it->getValue() == "object_decl");
+    ++it;
+    QVERIFY(it->getValue() == "SHAPE");
+    ++it;
+    QVERIFY(it->getValue() == "circle");
+    ++it;
+    QVERIFY(it->getValue() == "ID");
+    ++it;
+    QVERIFY(it->getValue() == "circleID");
+    ++it;
+    QVERIFY(it->getValue() == "{");
+
+    ++it;
+    QVERIFY(it->getValue() == "property");
+    ++it;
+    QVERIFY(it->getValue() == "PROPERTY_KEY");
+    ++it;
+    QVERIFY(it->getValue() == "text");
+    ++it;
+    QVERIFY(it->getValue() == "=");
+    ++it;
+    QVERIFY(it->getValue() == "TEXT");
+    ++it;
+    QVERIFY(it->getValue() == "circleTEXT");
+    ++it;
+    QVERIFY(it->getValue() == ";");
+
+    ++it;
+    QVERIFY(it->getValue() == "property");
+    ++it;
+    QVERIFY(it->getValue() == "PROPERTY_KEY");
+    ++it;
+    QVERIFY(it->getValue() == "сolor");
+    ++it;
+    QVERIFY(it->getValue() == "=");
+    ++it;
+    QVERIFY(it->getValue() == "NUMBER");
+    ++it;
+    QVERIFY(it->getValue() == "0");
+    ++it;
+    QVERIFY(it->getValue() == ";");
+
+    ++it;
+    QVERIFY(it->getValue() == "property");
+    ++it;
+    QVERIFY(it->getValue() == "PROPERTY_KEY");
+    ++it;
+    QVERIFY(it->getValue() == "border");
+    ++it;
+    QVERIFY(it->getValue() == "=");
+    ++it;
+    QVERIFY(it->getValue() == "NUMBER");
+    ++it;
+    QVERIFY(it->getValue() == "1");
+    ++it;
+    QVERIFY(it->getValue() == ";");
+
+    ++it;
+    QVERIFY(it->getValue() == "property");
+    ++it;
+    QVERIFY(it->getValue() == "PROPERTY_KEY");
+    ++it;
+    QVERIFY(it->getValue() == "size_text");
+    ++it;
+    QVERIFY(it->getValue() == "=");
+    ++it;
+    QVERIFY(it->getValue() == "NUMBER");
+    ++it;
+    QVERIFY(it->getValue() == "10");
+    ++it;
+    QVERIFY(it->getValue() == ";");
+
+    ++it;
+    QVERIFY(it->getValue() == "property");
+    ++it;
+    QVERIFY(it->getValue() == "PROPERTY_KEY");
+    ++it;
+    QVERIFY(it->getValue() == "x");
+    ++it;
+    QVERIFY(it->getValue() == "=");
+    ++it;
+    QVERIFY(it->getValue() == "NUMBER");
+    ++it;
+    QVERIFY(it->getValue() == "1");
+    ++it;
+    QVERIFY(it->getValue() == ";");
+
+    ++it;
+    QVERIFY(it->getValue() == "property");
+    ++it;
+    QVERIFY(it->getValue() == "PROPERTY_KEY");
+    ++it;
+    QVERIFY(it->getValue() == "y");
+    ++it;
+    QVERIFY(it->getValue() == "=");
+    ++it;
+    QVERIFY(it->getValue() == "NUMBER");
+    ++it;
+    QVERIFY(it->getValue() == "1");
+    ++it;
+    QVERIFY(it->getValue() == ";");
+
+    ++it;
+    QVERIFY(it->getValue() == "property");
+    ++it;
+    QVERIFY(it->getValue() == "PROPERTY_KEY");
+    ++it;
+    QVERIFY(it->getValue() == "radius");
+    ++it;
+    QVERIFY(it->getValue() == "=");
+    ++it;
+    QVERIFY(it->getValue() == "NUMBER");
+    ++it;
+    QCOMPARE(std::stod(it->getValue()), (double)1);
+    ++it;
+    QVERIFY(it->getValue() == ";");
+
+
+    ++it;
+    QVERIFY(it->getValue() == "}");
+
+    ++it;
+    QVERIFY(it->getValue() == "ENDGRAPH");
 }
+
+
+QTEST_GUILESS_MAIN(TestClass)
+#include "RetranslatorTest.moc"
