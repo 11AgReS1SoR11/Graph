2f99f455e4c3943e948ce8bc2410e87503b73046
maxonic228 <kromachmax2003@gmail.com>
2025-04-14 22:46:25 +0300

    Fix bags

--- Common/Parser/test/test.cpp
+++ Common/Parser/test/test.cpp
@@ -9,6 +9,7 @@ TEST_CASE("test1 object_decl ", "[Parser]")
     LOG_INFO(PARSER_LOG, "TEST CASE1:")
     Parser parser("tests/test1");
     std::unique_ptr<AST::ASTTree> astTree = parser.parse();
+    REQUIRE(astTree);
     LOG_INFO(PARSER_LOG, "TEST COMPLETE SUCCESFUL!\n\n")
 }
 
@@ -18,6 +19,7 @@ TEST_CASE("test2 object_decl ", "[Parser]")
     LOG_INFO(PARSER_LOG, "TEST CASE2:")
     Parser parser("tests/test2");
     std::unique_ptr<AST::ASTTree> astTree = parser.parse();
+    REQUIRE(astTree);
     LOG_INFO(PARSER_LOG, "TEST COMPLETE SUCCESFUL!\n\n")
 }
 
@@ -27,6 +29,7 @@ TEST_CASE("test3 object_decl/relation ", "[Parser]")
     LOG_INFO(PARSER_LOG, "TEST CASE3:")
     Parser parser("tests/test3");
     std::unique_ptr<AST::ASTTree> astTree = parser.parse();
+    REQUIRE(astTree);
     LOG_INFO(PARSER_LOG, "TEST COMPLETE SUCCESFUL!\n\n")
 }
 
@@ -36,6 +39,7 @@ TEST_CASE("test4 object_decl/relation/note ", "[Parser]")
     LOG_INFO(PARSER_LOG, "TEST CASE4:")
     Parser parser("tests/test4");
     std::unique_ptr<AST::ASTTree> astTree = parser.parse();
+    REQUIRE(astTree);
     LOG_INFO(PARSER_LOG, "TEST COMPLETE SUCCESFUL!\n\n")
 }
 
@@ -45,6 +49,7 @@ TEST_CASE("test5 graph ", "[Parser]")
     LOG_INFO(PARSER_LOG, "TEST CASE5:")
     Parser parser("tests/test5");
     std::unique_ptr<AST::ASTTree> astTree = parser.parse();
+    REQUIRE(astTree);
     LOG_INFO(PARSER_LOG, "TEST COMPLETE SUCCESFUL!\n\n")
 }
 
@@ -54,5 +59,6 @@ TEST_CASE("test6 dot_cloud ", "[Parser]")
     LOG_INFO(PARSER_LOG, "TEST CASE6:")
     Parser parser("tests/test6");
     std::unique_ptr<AST::ASTTree> astTree = parser.parse();
+    REQUIRE(astTree);
     LOG_INFO(PARSER_LOG, "TEST COMPLETE SUCCESFUL!\n\n")
 }
